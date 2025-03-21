cdd09ab36f227dd993b649a0e625b4196b4e4146
yamaguchitakashi <yamaguchi@bitbeans.com>
2025-03-21 17:56:26 +0900

    day2

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import {Main} from "../components/Main";
 import {Footer} from "../components/Footer";
 
-export function About() {
+export default function About() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Main title="About" page="about"/>
+      <Main page="about" />
       <Footer />
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import {Main} from "./components/Main";
 import {Footer} from "./components/Footer";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Main title="Home" page="home" />
+      <Main page="home" />
       <Footer />
     </div>
   );
